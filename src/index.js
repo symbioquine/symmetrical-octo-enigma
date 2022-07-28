04431dfe286fcecdf8b7fd496f7b44e1b83755c3
Symbioquine <symbioquine@gmail.com>
2022-07-27 17:12:12 -0700

    Fix misc style/formatting issues

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import BaseObject from 'ol/Object';
 import Control from 'ol/control/Control';
 import { CLASS_CONTROL, CLASS_UNSELECTABLE } from 'ol/css';
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
-import { Vector as VectorSource } from 'ol/source';
-import { Vector as VectorLayer } from 'ol/layer';
-import Geolocation from 'ol/Geolocation';
 import { MapInstanceManager } from '@farmos.org/farmos-map';
 
-import 'farm_map_google/src/main.js';
+import 'farm_map_google/src/main';
 
 import './styles.css';
 
@@ -17,58 +10,53 @@ const farmOSMap = new MapInstanceManager();
 
 farmOSMap.behaviors.google = window.farmOS.map.behaviors.google;
 
-const map = farmOSMap.create('farm-map', { units: 'us' });
+const pageMap = farmOSMap.create('farm-map', { units: 'us' });
 
-const drawingLayer = map.addLayer('vector');
+const drawingLayer = pageMap.addLayer('vector');
 
-map.addBehavior('edit', { layer: drawingLayer }).then(() => {
-  map.addBehavior('measure', { layer: drawingLayer });
+pageMap.addBehavior('edit', { layer: drawingLayer }).then(() => {
+  pageMap.addBehavior('measure', { layer: drawingLayer });
 });
 
-const getGeolocationObject = () => {
-  return new Promise((resolve, fail) => {
-    const findGeolocateCtrl = () => map.map.getControls().getArray().find(c => c.element.className.indexOf('ol-geolocate') !== -1);
+const getGeolocationObject = () => new Promise((resolve) => {
+  const findGeolocateCtrl = () => pageMap.map.getControls().getArray().find((c) => c.element.className.indexOf('ol-geolocate') !== -1);
 
-    let geolocateCtrl = findGeolocateCtrl();
+  let geolocateCtrl = findGeolocateCtrl();
 
-    const awaitActivated = () => {
-      if (geolocateCtrl.element.className.indexOf('active') !== -1) {
-        resolve(geolocateCtrl.geolocation);
-      }
+  const awaitActivated = () => {
+    if (geolocateCtrl.element.className.indexOf('active') !== -1) {
+      resolve(geolocateCtrl.geolocation);
+    }
 
-      const attrObserver = new MutationObserver((mutations) => {
-        mutations.forEach(mu => {
-          if (mu.type !== "attributes" || mu.attributeName !== "class") {
-            return;
-          }
-          if (geolocateCtrl.element.className.indexOf('active') !== -1) {
-            attrObserver.disconnect();
-            resolve(geolocateCtrl.geolocation);
-          }
-        });
-      });
-
-      attrObserver.observe(geolocateCtrl.element, { attributes: true });
-    };
-
-    if (geolocateCtrl) {
-      awaitActivated();
-    } else {
-      const onMapAddCtrl = map.map.getControls().on('add', () => {
-        geolocateCtrl = findGeolocateCtrl();
-        if (geolocateCtrl) {
-          map.map.getControls().un('add', onMapAddCtrl.listener);
-          awaitActivated();
+    const attrObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mu) => {
+        if (mu.type !== 'attributes' || mu.attributeName !== 'class') {
+          return;
+        }
+        if (geolocateCtrl.element.className.indexOf('active') !== -1) {
+          attrObserver.disconnect();
+          resolve(geolocateCtrl.geolocation);
         }
       });
-    }
-    
-    
-  });
-};
+    });
+
+    attrObserver.observe(geolocateCtrl.element, { attributes: true });
+  };
+
+  if (geolocateCtrl) {
+    awaitActivated();
+  } else {
+    const onMapAddCtrl = pageMap.map.getControls().on('add', () => {
+      geolocateCtrl = findGeolocateCtrl();
+      if (geolocateCtrl) {
+        pageMap.map.getControls().un('add', onMapAddCtrl.listener);
+        awaitActivated();
+      }
+    });
+  }
+});
 
 class GeolocationDrawing extends Control {
-
   /**
    * @param {Options=} opts GeolocationDrawing options.
    */
@@ -91,6 +79,7 @@ class GeolocationDrawing extends Control {
 
     this.innerControlElements = {};
 
+    /* eslint-disable no-param-reassign */
     function createControlElement(elementTag, name, builderFn) {
       const controlElem = document.createElement(elementTag);
       controlElem.className = `${className} ${name}`;
@@ -158,7 +147,7 @@ class GeolocationDrawing extends Control {
       // When interactions are added or removed update our internal state
       // to track the active drawing interaction/feature - if any
       const trackDrawingInteractions = () => {
-        const drawingInteraction = map.getInteractions().getArray().find(i => typeof i.finishDrawing === 'function');
+        const drawingInteraction = map.getInteractions().getArray().find((i) => typeof i.finishDrawing === 'function');
 
         if (this.activeDrawingInteraction) {
           this.clearActiveDrawingListeners();
@@ -177,11 +166,11 @@ class GeolocationDrawing extends Control {
             this.innerControlElements.closeFeatureButton.classList.remove('hidden');
           }
         });
-        this.onDrawAbort = this.activeDrawingInteraction.on('drawabort', (drawEvt) => {
+        this.onDrawAbort = this.activeDrawingInteraction.on('drawabort', () => {
           this.activeDrawingFeature = undefined;
           this.innerControlElements.closeFeatureButton.classList.add('hidden');
         });
-        this.onDrawEnd = this.activeDrawingInteraction.on('drawend', (drawEvt) => {
+        this.onDrawEnd = this.activeDrawingInteraction.on('drawend', () => {
           this.activeDrawingFeature = undefined;
           this.innerControlElements.closeFeatureButton.classList.add('hidden');
         });
@@ -196,10 +185,11 @@ class GeolocationDrawing extends Control {
   clearActiveDrawingListeners() {
     const clearListener = (eventsKeyAttrName) => {
       if (this[eventsKeyAttrName]) {
-        this[eventsKeyAttrName].target.un(this[eventsKeyAttrName].type, this[eventsKeyAttrName].listener);
+        this[eventsKeyAttrName].target
+          .un(this[eventsKeyAttrName].type, this[eventsKeyAttrName].listener);
         this[eventsKeyAttrName] = undefined;
       }
-    }
+    };
 
     clearListener('onDrawStart');
     clearListener('onDrawAbort');
@@ -214,15 +204,15 @@ class GeolocationDrawing extends Control {
     }
 
     const drawInteractions = this.getMap().getInteractions().getArray()
-      .filter(interaction => typeof interaction.finishDrawing === 'function');
+      .filter((interaction) => typeof interaction.finishDrawing === 'function');
 
     // If we're not already drawing, then simulate a click on the edit control's "point" button
     if (!drawInteractions.length) {
-      const editControl = this.getMap().getControls().getArray().find(c => c.element.className.indexOf('ol-edit') !== -1);
+      const editControl = this.getMap().getControls().getArray().find((c) => c.element.className.indexOf('ol-edit') !== -1);
 
       if (editControl) {
-        const evt = new Event('click', { bubbles: true } );
-        editControl.buttons.point.dispatchEvent( evt );
+        const evt = new Event('click', { bubbles: true });
+        editControl.buttons.point.dispatchEvent(evt);
       }
     }
 
@@ -231,12 +221,14 @@ class GeolocationDrawing extends Control {
 
   captureSinglePoint() {
     const drawInteractions = this.getMap().getInteractions().getArray()
-      .filter(interaction => typeof interaction.finishDrawing === 'function');
+      .filter((interaction) => typeof interaction.finishDrawing === 'function');
 
     const pos = this.geolocation.getPosition();
 
     if (drawInteractions.length) {
       drawInteractions[0].appendCoordinates([pos]);
+
+      /* eslint-disable no-underscore-dangle */
       if (drawInteractions[0].mode_ === 'Point') {
         drawInteractions[0].finishDrawing();
       }
@@ -261,15 +253,15 @@ class GeolocationDrawing extends Control {
     this.innerControlElements.streamPointButton.classList.add('active');
 
     const drawInteractions = this.getMap().getInteractions().getArray()
-      .filter(interaction => typeof interaction.finishDrawing === 'function');
+      .filter((interaction) => typeof interaction.finishDrawing === 'function');
 
     // If we're not already drawing, then simulate a click on the edit control's "polygon" button
     if (!drawInteractions.length) {
-      const editControl = this.getMap().getControls().getArray().find(c => c.element.className.indexOf('ol-edit') !== -1);
+      const editControl = this.getMap().getControls().getArray().find((c) => c.element.className.indexOf('ol-edit') !== -1);
 
       if (editControl) {
-        const evt = new Event('click', { bubbles: true } );
-        editControl.buttons.polygon.dispatchEvent( evt );
+        const evt = new Event('click', { bubbles: true });
+        editControl.buttons.polygon.dispatchEvent(evt);
       }
     }
 
@@ -287,7 +279,7 @@ class GeolocationDrawing extends Control {
     }
 
     const drawInteractions = this.getMap().getInteractions().getArray()
-      .filter(interaction => typeof interaction.finishDrawing === 'function');
+      .filter((interaction) => typeof interaction.finishDrawing === 'function');
 
     if (drawInteractions.length && finishDrawing) {
       drawInteractions[0].finishDrawing();
@@ -295,46 +287,58 @@ class GeolocationDrawing extends Control {
   }
 }
 
-(async function() {
+(async function main() {
   const geolocation = await getGeolocationObject();
 
   const control = new GeolocationDrawing({ geolocation });
 
   if (geolocation.getTracking()) {
-    map.map.addControl(control);
+    pageMap.map.addControl(control);
   }
 
   geolocation.on('change:tracking', () => {
     if (geolocation.getTracking()) {
-      map.map.addControl(control);
+      pageMap.map.addControl(control);
     } else {
-      map.map.removeControl(control);
+      pageMap.map.removeControl(control);
     }
   });
-})();
+}());
 
-map.addBehavior("sidePanel").then(async () => {
-  const locationInfoPane = map.sidePanel.definePane({
+// Helper to make defining elements easier
+const el = (tagName, attrs, fn) => {
+  const e = document.createElement(tagName);
+  Object.entries(attrs || {}).forEach(([key, value]) => e.setAttribute(key, value));
+  if (fn) fn(e);
+  return e;
+};
+
+// From: https://stackoverflow.com/a/1026087/1864479
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+pageMap.addBehavior('sidePanel').then(async () => {
+  const locationInfoPane = pageMap.sidePanel.definePane({
     paneId: 'location_info',
     name: 'Location Info',
     icon: '♞',
     weight: 80,
   });
 
-  const instructionsWidget = locationInfoPane.addWidgetElement(el('span', {}, instructionsSpan => {
-    instructionsSpan.innerHTML = "First click the geolocate button to see location info here.";
+  const instructionsWidget = locationInfoPane.addWidgetElement(el('span', {}, (instructionsSpan) => {
+    instructionsSpan.innerHTML = 'First click the geolocate button to see location info here.';
   }));
 
   const geolocation = await getGeolocationObject();
 
   instructionsWidget.remove();
 
-  locationInfoPane.addWidgetElement(el('p', { }, infoStatsP => {
-
+  locationInfoPane.addWidgetElement(el('p', { }, (infoStatsP) => {
     const defineStatsFacet = (facetId, label, format) => {
-      infoStatsP.appendChild(el('div', {}, div => {
+      infoStatsP.appendChild(el('div', {}, (div) => {
         div.innerHTML = `${label}:`;
-        div.appendChild(el('code', {}, facet => {
+        div.appendChild(el('code', {}, (facet) => {
           const updateFacetInnerText = () => {
             let facetGetterFn = geolocation[`get${capitalizeFirstLetter(facetId)}`];
             if (typeof facetGetterFn !== 'function') {
@@ -352,15 +356,15 @@ map.addBehavior("sidePanel").then(async () => {
             if (typeof format === 'function') {
               formatFn = format;
             } else if (typeof facetValue === 'number') {
-              formatFn = (v) => v.toFixed(3) + ' ' + format;
+              formatFn = (v) => `${v.toFixed(3)} ${format}`;
             } else {
-              formatFn = (v) => JSON.stringify(v) + ' ' + format;
+              formatFn = (v) => `${JSON.stringify(v)} ${format}`;
             }
 
             facet.innerText = formatFn(facetValue);
-          }
+          };
           updateFacetInnerText();
-          geolocation.on(`change:${facetId}`, evt => updateFacetInnerText());
+          geolocation.on(`change:${facetId}`, () => updateFacetInnerText());
         }));
       }));
     };
@@ -372,58 +376,10 @@ map.addBehavior("sidePanel").then(async () => {
     defineStatsFacet('heading', 'heading', '[rad]');
     defineStatsFacet('speed', 'speed', '[m/s]');
 
-    locationInfoPane.addWidgetElement(el('div', { id: 'info'}, locationErrorsDiv => {
-      geolocation.on('error', function (error) {
+    locationInfoPane.addWidgetElement(el('div', { id: 'info' }, (locationErrorsDiv) => {
+      geolocation.on('error', (error) => {
         locationErrorsDiv.innerHTML += error.message;
       });
     }));
-
   }));
-
-  const accuracyFeature = new Feature();
-  geolocation.on('change:accuracyGeometry', function () {
-    accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
-  });
-
-  const positionFeature = new Feature();
-  positionFeature.setStyle(
-    new Style({
-      image: new CircleStyle({
-        radius: 6,
-        fill: new Fill({
-          color: '#3399CC',
-        }),
-        stroke: new Stroke({
-          color: '#fff',
-          width: 2,
-        }),
-      }),
-    })
-  );
-
-  geolocation.on('change:position', function () {
-    const coordinates = geolocation.getPosition();
-    positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
-  });
-
-  new VectorLayer({
-    map: map.map,
-    source: new VectorSource({
-      features: [accuracyFeature, positionFeature],
-    }),
-  });
-
 });
-
-// Helper to make defining elements easier
-const el = (tagName, attrs, fn) => {
-  const e = document.createElement(tagName);
-  Object.entries(attrs || {}).forEach(([key, value]) => e.setAttribute(key, value));
-  if (fn) fn(e);
-  return e;
-};
-
-// From: https://stackoverflow.com/a/1026087/1864479
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
